51ff953b211644c2a833fddc8b502318fbce0fbf
Nikolay Lembinen <homafromptz@gmail.com>
2016-07-22 06:03:29 +0300

    Check Laravel migration system and make few changes in DB

---
+++ build/backend_php_dist/lrvl_mvc/database/migrations/2016_07_22_025459_alter_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar',255)->after('password');
+            $table->string('background',255)->after('avatar');
+            $table->longtext('description')->after('background');
+            $table->boolean('activated')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
