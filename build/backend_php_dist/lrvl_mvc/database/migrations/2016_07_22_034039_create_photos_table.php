b1a8770a17c80bbeae2c9ec579d6005df9bb0c50
Nikolay Lembinen <homafromptz@gmail.com>
2016-07-22 07:03:09 +0300

    Create all tables and model templates

---
+++ build/backend_php_dist/lrvl_mvc/database/migrations/2016_07_22_034039_create_photos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',255);
+            $table->longtext('description');
+            $table->string('img',255);
+            $table->integer('album_id')->unsigned();
+            $table->foreign('album_id')->references('id')->on('albums');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('photos');
+    }
+}
---
+++ build/backend_php_dist/lrvl_mvc/database/migrations/2016_07_22_034829_create_likes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLikesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('photo_id')->unsigned();
+            $table->primary(array('user_id','photo_id'));
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('photo_id')->references('id')->on('photos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('likes');
+    }
+}
