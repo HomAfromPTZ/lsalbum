270e4d23b481e16faaa89a3dd453250d60e1887b
Andrey Semenov <andrey.semenoff@gmail.com>
2016-08-04 09:41:50 +0300

    Merge php final

--- build/backend_php_dist/lrvl_mvc/resources/views/album.blade.php
+++ build/backend_php_dist/lrvl_mvc/resources/views/album.blade.php
@@ -63,77 +63,9 @@
 						<button class="btn btn_animated"><i class="fa fa-plus"></i><span>Добавить</span></button>
 					</div>
 					<div class="album-container">
-<<<<<<< HEAD
-
-            @forelse ($photos as $photo)
-              <div class="photo-item single-photo"
-                   data-id="{{ $photo->id }}"
-                   data-title="{{ $photo->title }}"
-                   data-desc="{{ $photo->description }}"
-                   data-likes="{{ $photo->likes }}"
-                   data-comments="{{ $photo->comments }}"
-                   data-thumb="{{ $photo->thumb_url }}"
-                   data-user_id="{{ $photo->user->id }}"
-                   data-user_avatar="{{ $photo->user->avatar }}"
-                   data-user_name="{{ $photo->user->name }}">
-                <div class="photo-item-holder">
-                  <div class="album-photo">
-                    <a href="#" class="open-img-popup js-open-slider">
-                      <div class="album-mask"><i class="fa fa-search-plus"></i></div>
-                      <div style="background-image: url('{{ $photo->thumb_url }}')" class="album-photo__thumb"></div>
-                    </a>
-                    <div class="photo-info">
-                      <button class="photo-info__item">
-                        <i class="fa fa-commenting"> </i>
-                        <span class="comment-count">{{ $photo->comments }}</span>
-                      </button>
-                      <button class="photo-info__item">
-                        <i class="fa fa-heart"> </i>
-                        <span class="like-count">{{ $photo->likes }}</span>
-                      </button>
-                    </div>
-                  </div>
-                  <div class="album-category">
-                    <button class="edit-post js-edit-photo">
-                      <i class="fa fa-pencil"></i>
-                    </button>
-                    <span class="category-name">{{ $photo->title }}</span>
-                  </div>
-                </div>
-
-                <div class="is-hidden comments_hidden">
-                  
-                  @forelse ($photo->comment as $comment)
-                    <div class="comments__item">
-                      <div class="comments__item-photo">
-                        <div class="photo-user-img">
-                          <img src="/assets/img/default_avatar.jpg" alt="{{ $comment->user->name }}"/>
-                          <a href="/user/{{ $comment->user->id }}" class="photo-user-img__mask">
-                            <svg class="svg-more">
-                              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-more"></use>
-                            </svg>
-                          </a>
-                        </div>
-                      </div>
-                      <div class="comments__item-info">
-                        <div class="user__name">{{ $comment->user->name }}</div>
-                        <div class="comments__item-text">{{ $comment->content }}</div>
-                      </div>
-                    </div>
-                  @empty
-                    Нет комментариев
-                  @endforelse
-
-                </div>
-              </div>
-            @empty
-              <h3>Нет фотографий</h3>
-            @endforelse
-
-
-=======
+						
 						@include('_common.photos-list')
->>>>>>> 4cb1948e43f386317e6305d2ccfc8f69b4ba4dc4
+					
 					</div>
 				</div>
 			</div>
